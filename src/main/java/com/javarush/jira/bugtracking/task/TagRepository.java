96dde5572098dc06c057f8d9430f3eb97a886433
UBCh <u2b127@gmail.com>
2023-08-27 21:59:59 +0300

    add docker-compose.yml

--- src/main/java/com/javarush/jira/bugtracking/task/TagRepository.java
+++ src/main/java/com/javarush/jira/bugtracking/task/TagRepository.java
@@ -1,13 +1,14 @@
 package com.javarush.jira.bugtracking.task;
 
 import com.javarush.jira.common.BaseRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-
+@Repository
 @Transactional(readOnly = true)
 public interface TagRepository extends BaseRepository<Tag> {
 
