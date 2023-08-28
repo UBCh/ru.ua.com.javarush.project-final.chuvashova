d105fe87fdaeca8086f461ecca1ed7fa93b6abee
UBCh <u2b127@gmail.com>
2023-08-28 22:47:33 +0300

    completed tasks:
    - Delete social networks: VK, Yandex.
    - Move sensor information to a separate properties file (+application-test.yaml)
    - Write tests for all public methods of the ProfileRestController controller
    - Refactor the com.javarush.jira.bugtracking.attachment.FileUtil#upload method
    - Add new functionality: adding tags to the task (REST API + delivery on the service)
    - Write a Dockerfile for the underlying server
    - Write a docker-compose file to run the server container along with the database and nginx.

---
+++ src/main/resources/db/changelogH2.sql
@@ -0,0 +1,320 @@
+CREATE SCHEMA testdb;
+
+create table PROJECT
+(
+    ID          bigint primary key,
+    CODE        varchar(32)   not null
+        constraint UK_PROJECT_CODE unique,
+    TITLE       varchar(1024) not null,
+    DESCRIPTION varchar(4096) not null,
+    TYPE_CODE   varchar(32)   not null,
+    STARTPOINT  timestamp,
+    ENDPOINT    timestamp,
+    PARENT_ID   bigint,
+    constraint FK_PROJECT_PARENT foreign key (PARENT_ID) references PROJECT (ID) on delete cascade
+);
+
+create table MAIL_CASE
+(
+    ID        bigint primary key,
+    EMAIL     varchar(255) not null,
+    NAME      varchar(255) not null,
+    DATE_TIME timestamp    not null,
+    RESULT    varchar(255) not null,
+    TEMPLATE  varchar(255) not null
+);
+
+create table SPRINT
+(
+    ID          bigint primary key,
+    STATUS_CODE varchar(32)   not null,
+    STARTPOINT  timestamp,
+    ENDPOINT    timestamp,
+    TITLE       varchar(1024) not null,
+    PROJECT_ID  bigint        not null,
+    constraint FK_SPRINT_PROJECT foreign key (PROJECT_ID) references PROJECT (ID) on delete cascade
+);
+
+create table REFERENCE
+(
+    ID         bigint primary key,
+    CODE       varchar(32)   not null,
+    REF_TYPE   smallint      not null,
+    ENDPOINT   timestamp,
+    STARTPOINT timestamp,
+    TITLE      varchar(1024) not null,
+    AUX        varchar,
+    constraint UK_REFERENCE_REF_TYPE_CODE unique (REF_TYPE, CODE)
+);
+
+create table USERS
+(
+    ID           bigint primary key,
+    DISPLAY_NAME varchar(32)  not null
+        constraint UK_USERS_DISPLAY_NAME unique,
+    EMAIL        varchar(128) not null
+        constraint UK_USERS_EMAIL unique,
+    FIRST_NAME   varchar(32)  not null,
+    LAST_NAME    varchar(32),
+    PASSWORD     varchar(128) not null,
+    ENDPOINT     timestamp,
+    STARTPOINT   timestamp
+);
+
+create table PROFILE
+(
+    ID                 bigint primary key,
+    LAST_LOGIN         timestamp,
+    LAST_FAILED_LOGIN  timestamp,
+    MAIL_NOTIFICATIONS bigint,
+    constraint FK_PROFILE_USERS foreign key (ID) references USERS (ID) on delete cascade
+);
+
+create table CONTACT
+(
+    ID    bigint primary key,
+    CODE  varchar(32)  not null,
+    VALUE varchar(256) not null,
+
+    constraint FK_CONTACT_PROFILE foreign key (ID) references PROFILE (ID) on delete cascade
+);
+
+create table TASK
+(
+    ID            bigint primary key,
+    TITLE         varchar(1024) not null,
+    DESCRIPTION   varchar(4096) not null,
+    TYPE_CODE     varchar(32)   not null,
+    STATUS_CODE   varchar(32)   not null,
+    PRIORITY_CODE varchar(32)   not null,
+    ESTIMATE      integer,
+    UPDATED       timestamp,
+    PROJECT_ID    bigint        not null,
+    SPRINT_ID     bigint,
+    PARENT_ID     bigint,
+    STARTPOINT    timestamp,
+    ENDPOINT      timestamp,
+    constraint FK_TASK_SPRINT foreign key (SPRINT_ID) references SPRINT (ID) on delete set null,
+    constraint FK_TASK_PROJECT foreign key (PROJECT_ID) references PROJECT (ID) on delete cascade,
+    constraint FK_TASK_PARENT_TASK foreign key (PARENT_ID) references TASK (ID) on delete cascade
+);
+
+create table ACTIVITY
+(
+    ID            bigint primary key,
+    AUTHOR_ID     bigint not null,
+    TASK_ID       bigint not null,
+    UPDATED       timestamp,
+    COMMENT       varchar(4096),
+--     history of task field change
+    TITLE         varchar(1024),
+    DESCRIPTION   varchar(4096),
+    ESTIMATE      integer,
+    TYPE_CODE     varchar(32),
+    STATUS_CODE   varchar(32),
+    PRIORITY_CODE varchar(32),
+    constraint FK_ACTIVITY_USERS foreign key (AUTHOR_ID) references USERS (ID),
+    constraint FK_ACTIVITY_TASK foreign key (TASK_ID) references TASK (ID) on delete cascade
+);
+
+create table TASK_TAG
+(
+    ID      bigint      not null,
+    TASK_ID bigint      not null,
+    TAG     varchar(32) not null,
+    constraint UK_TASK_TAG unique (TASK_ID, TAG),
+    constraint FK_TASK_TAG foreign key (TASK_ID) references TASK (ID) on delete cascade
+);
+
+create table USER_BELONG
+(
+    ID             bigint primary key,
+    OBJECT_ID      bigint      not null,
+    OBJECT_TYPE    smallint    not null,
+    USER_ID        bigint      not null,
+    USER_TYPE_CODE varchar(32) not null,
+    STARTPOINT     timestamp,
+    ENDPOINT       timestamp,
+    constraint FK_USER_BELONG foreign key (USER_ID) references USERS (ID)
+);
+create unique index UK_USER_BELONG on USER_BELONG (OBJECT_ID, OBJECT_TYPE, USER_ID, USER_TYPE_CODE);
+create index IX_USER_BELONG_USER_ID on USER_BELONG (USER_ID);
+
+create table ATTACHMENT
+(
+    ID          bigint primary key,
+    NAME        varchar(128)  not null,
+    FILE_LINK   varchar(2048) not null,
+    OBJECT_ID   bigint        not null,
+    OBJECT_TYPE smallint      not null,
+    USER_ID     bigint        not null,
+    DATE_TIME   timestamp,
+    constraint FK_ATTACHMENT foreign key (USER_ID) references USERS (ID)
+);
+
+create table USER_ROLE
+(
+    USER_ID bigint   not null,
+    ROLE    smallint not null,
+    constraint UK_USER_ROLE unique (USER_ID, ROLE),
+    constraint FK_USER_ROLE foreign key (USER_ID) references USERS (ID) on delete cascade
+);
+
+--changeset kmpk:populate_data
+--============ References =================
+insert into REFERENCE (CODE, TITLE, REF_TYPE)
+-- TASK
+values ('task', 'Task', 2),
+       ('story', 'Story', 2),
+       ('bug', 'Bug', 2),
+       ('epic', 'Epic', 2),
+-- SPRINT_STATUS
+       ('planning', 'Planning', 4),
+       ('active', 'Active', 4),
+       ('finished', 'Finished', 4),
+-- USER_TYPE
+       ('author', 'Author', 5),
+       ('developer', 'Developer', 5),
+       ('reviewer', 'Reviewer', 5),
+       ('tester', 'Tester', 5),
+-- PROJECT
+       ('scrum', 'Scrum', 1),
+       ('task_tracker', 'Task tracker', 1),
+-- CONTACT
+       ('skype', 'Skype', 0),
+       ('tg', 'Telegram', 0),
+       ('mobile', 'Mobile', 0),
+       ('phone', 'Phone', 0),
+       ('website', 'Website', 0),
+       ('vk', 'VK', 0),
+       ('linkedin', 'LinkedIn', 0),
+       ('github', 'GitHub', 0),
+-- PRIORITY
+       ('critical', 'Critical', 7),
+       ('high', 'High', 7),
+       ('normal', 'Normal', 7),
+       ('low', 'Low', 7),
+       ('neutral', 'Neutral', 7);
+
+insert into REFERENCE (CODE, TITLE, REF_TYPE, AUX)
+-- MAIL_NOTIFICATION
+values ('assigned', 'Assigned', 6, '1'),
+       ('three_days_before_deadline', 'Three days before deadline', 6, '2'),
+       ('two_days_before_deadline', 'Two days before deadline', 6, '4'),
+       ('one_day_before_deadline', 'One day before deadline', 6, '8'),
+       ('deadline', 'Deadline', 6, '16'),
+       ('overdue', 'Overdue', 6, '32'),
+-- TASK_STATUS
+       ('todo', 'ToDo', 3, 'in_progress,canceled'),
+       ('in_progress', 'In progress', 3, 'ready_for_review,canceled'),
+       ('ready_for_review', 'Ready for review', 3, 'review,canceled'),
+       ('review', 'Review', 3, 'in_progress,ready_for_test,canceled'),
+       ('ready_for_test', 'Ready for test', 3, 'test,canceled'),
+       ('test', 'Test', 3, 'done,in_progress,canceled'),
+       ('done', 'Done', 3, 'canceled'),
+       ('canceled', 'Canceled', 3, null);
+
+--changeset gkislin:change_backtracking_tables
+
+insert into REFERENCE (CODE, TITLE, REF_TYPE, AUX)
+values ('todo', 'ToDo', 3, 'in_progress,canceled'),
+       ('in_progress', 'In progress', 3, 'ready_for_review,canceled'),
+       ('ready_for_review', 'Ready for review', 3, 'in_progress,review,canceled'),
+       ('review', 'Review', 3, 'in_progress,ready_for_test,canceled'),
+       ('ready_for_test', 'Ready for test', 3, 'review,test,canceled'),
+       ('test', 'Test', 3, 'done,in_progress,canceled'),
+       ('done', 'Done', 3, 'canceled'),
+       ('canceled', 'Canceled', 3, null);
+
+--changeset gkislin:users_add_on_delete_cascade
+
+insert into REFERENCE (CODE, TITLE, REF_TYPE)
+-- USER_TYPE
+values ('project_author', 'Author', 5),
+       ('project_manager', 'Manager', 5),
+       ('sprint_author', 'Author', 5),
+       ('sprint_manager', 'Manager', 5),
+       ('task_author', 'Author', 5),
+       ('task_developer', 'Developer', 5),
+       ('task_reviewer', 'Reviewer', 5),
+       ('task_tester', 'Tester', 5);
+
+insert into REFERENCE (CODE, TITLE, REF_TYPE, AUX)
+values ('todo', 'ToDo', 3, 'in_progress,canceled|'),
+       ('in_progress', 'In progress', 3, 'ready_for_review,canceled|task_developer'),
+       ('ready_for_review', 'Ready for review', 3, 'in_progress,review,canceled|'),
+       ('review', 'Review', 3, 'in_progress,ready_for_test,canceled|task_reviewer'),
+       ('ready_for_test', 'Ready for test', 3, 'review,test,canceled|'),
+       ('test', 'Test', 3, 'done,in_progress,canceled|task_tester'),
+       ('done', 'Done', 3, 'canceled|'),
+       ('canceled', 'Canceled', 3, null);
+
+--changeset ishlyakhtenkov:change_UK_USER_BELONG
+insert into USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, DISPLAY_NAME)
+values ('user@gmail.com', '{noop}password', 'userFirstName', 'userLastName', 'userDisplayName'),
+       ('admin@gmail.com', '{noop}admin', 'adminFirstName', 'adminLastName', 'adminDisplayName'),
+       ('guest@gmail.com', '{noop}guest', 'guestFirstName', 'guestLastName', 'guestDisplayName'),
+       ('manager@gmail.com', '{noop}manager', 'managerFirstName', 'managerLastName', 'managerDisplayName');
+
+-- 0 DEV
+-- 1 ADMIN
+-- 2 MANAGER
+
+insert into USER_ROLE (USER_ID, ROLE)
+values (1, 0),
+       (2, 0),
+       (2, 1),
+       (4, 2);
+
+insert into PROFILE (ID, LAST_FAILED_LOGIN, LAST_LOGIN, MAIL_NOTIFICATIONS)
+values (1, null, null, 49),
+       (2, null, null, 14);
+
+insert into CONTACT (ID, CODE, VALUE)
+values (1, 'skype', 'userSkype'),
+       (1, 'mobile', '+01234567890'),
+       (1, 'website', 'user.com'),
+       (2, 'github', 'adminGitHub'),
+       (2, 'tg', 'adminTg'),
+       (2, 'vk', 'adminVk');
+
+
+insert into PROJECT (code, title, description, type_code, parent_id)
+values ('PR1', 'PROJECT-1', 'test project 1', 'task_tracker', null),
+       ('PR2', 'PROJECT-2', 'test project 2', 'task_tracker', 1);
+
+insert into SPRINT (status_code, startpoint, endpoint, project_id)
+values ('finished', '2023-05-01 08:05:10', '2023-05-07 17:10:01', 1),
+       ('active', '2023-05-01 08:06:00', null, 1),
+       ('active', '2023-05-01 08:07:00', null, 1),
+       ('planning', '2023-05-01 08:08:00', null, 1),
+       ('active', '2023-05-10 08:06:00', null, 2),
+       ('planning', '2023-05-10 08:07:00', null, 2),
+       ('planning', '2023-05-10 08:08:00', null, 2);
+
+insert into TASK (TITLE, TYPE_CODE, STATUS_CODE, PROJECT_ID, SPRINT_ID, STARTPOINT)
+values ('Data', 'epic', 'in_progress', 1, 1, '2023-05-15 09:05:10'),
+       ('Trees', 'epic', 'in_progress', 1, 1, '2023-05-15 12:05:10'),
+       ('task-3', 'task', 'ready_for_test', 2, 5, '2023-06-14 09:28:10'),
+       ('task-4', 'task', 'ready_for_review', 2, 5, '2023-06-14 09:28:10'),
+       ('task-5', 'task', 'todo', 2, 5, '2023-06-14 09:28:10'),
+       ('task-6', 'task', 'done', 2, 5, '2023-06-14 09:28:10'),
+       ('task-7', 'task', 'canceled', 2, 5, '2023-06-14 09:28:10');
+
+
+insert into ACTIVITY(AUTHOR_ID, TASK_ID, UPDATED, COMMENT, TITLE, DESCRIPTION, ESTIMATE, TYPE_CODE, STATUS_CODE,
+                     PRIORITY_CODE)
+values (1, 1, '2023-05-15 09:05:10', null, 'Data', null, 3, 'epic', 'in_progress', 'low'),
+       (2, 1, '2023-05-15 12:25:10', null, 'Data', null, null, null, null, 'normal'),
+       (1, 1, '2023-05-15 14:05:10', null, 'Data', null, 4, null, null, null),
+       (1, 2, '2023-05-15 12:05:10', null, 'Trees', 'Trees desc', 4, 'epic', 'in_progress', 'normal');
+
+insert into USER_BELONG (OBJECT_ID, OBJECT_TYPE, USER_ID, USER_TYPE_CODE, STARTPOINT, ENDPOINT)
+values (1, 2, 2, 'task_developer', '2023-06-14 08:35:10', '2023-06-14 08:55:00'),
+       (1, 2, 2, 'task_reviewer', '2023-06-14 09:35:10', null),
+       (1, 2, 1, 'task_developer', '2023-06-12 11:40:00', '2023-06-12 12:35:00'),
+       (1, 2, 1, 'task_developer', '2023-06-13 12:35:00', null),
+       (1, 2, 1, 'task_tester', '2023-06-14 15:20:00', null),
+       (2, 2, 2, 'task_developer', '2023-06-08 07:10:00', null),
+       (2, 2, 1, 'task_developer', '2023-06-09 14:48:00', null),
+       (2, 2, 1, 'task_tester', '2023-06-10 16:37:00', null);
